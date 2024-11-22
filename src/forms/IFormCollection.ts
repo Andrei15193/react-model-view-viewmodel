c47d8a52092eca2c801c0a8c43700a2c9f19d6f5
Andrei Fangli <andrei_fangli@outlook.com>
2024-11-22 22:00:49 +0200

    Updated redundant extended interfaces

--- src/forms/IFormCollection.ts
+++ src/forms/IFormCollection.ts
@@ -1,7 +1,5 @@
 import type { IObservableCollection } from '../collections';
-import type { IValidatable } from '../validation';
 import type { Form } from './Form';
-import type { IConfigurableFormCollection } from './IConfigurableFormCollection';
 import type { IReadOnlyFormCollection } from './IReadOnlyFormCollection';
 
 /** 
@@ -11,7 +9,7 @@ import type { IReadOnlyFormCollection } from './IReadOnlyFormCollection';
  * @template TForm The concrete type of the form section.
  * @template TValidationError The concrete type for representing validation errors (strings, enums, numbers etc.).
  */
-export interface IFormCollection<TForm extends Form<TValidationError>, TValidationError = string> extends IValidatable<TValidationError>, IObservableCollection<TForm>, IConfigurableFormCollection<TForm, TValidationError>, IReadOnlyFormCollection<TForm, TValidationError> {
+export interface IFormCollection<TForm extends Form<TValidationError>, TValidationError = string> extends IObservableCollection<TForm>, IReadOnlyFormCollection<TForm, TValidationError> {
     /**
      * Gets or sets the number of items in the collection.
      * @see [Array.length](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/length)
